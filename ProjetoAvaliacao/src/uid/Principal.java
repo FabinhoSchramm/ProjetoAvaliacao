5e487362e01b62897eacf0b1c90be380b08b3fbf
Fabio Schramm <ff_schramm@hotmail.com>
2025-07-26 19:42:33 -0300

    nova logica facilitando o uso do programa

--- ProjetoAvaliacao/src/uid/Principal.java
+++ ProjetoAvaliacao/src/uid/Principal.java
@@ -3,14 +3,10 @@ package uid;
 public class Principal {
 
 	public static void main(String[] args) {
+
+		Aluno a1 = new Aluno("Mario", "Engenharia");
 		
-		NotaAleatoria n = new NotaAleatoria();
-		
-		Avaliacao luigi = new Avaliacao(n.notas()[0],n.notas()[1],n.notas()[2]);
-		
-		System.out.printf("Media luigi aritimetica: %.2f%n" , luigi.mediaAritimetica());
-		System.out.printf("Media luigi ponderada: %.2f%n" , luigi.mediaPonderada());
-		
+		a1.info();
 	}
 
 };
